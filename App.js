f82d8ca8b0c35bba39a3fbd125f249b4cacbed80
Riyad Shauk <riyad.shauk@gmail.com>
2019-01-31 21:58:44 -0800

    fix issue #25 & build issues, update to expo sdk 32, react 16.5, add yarn.lock

--- App.js
+++ App.js
@@ -75,12 +75,11 @@ export default class CameraScreen extends React.Component {
     showMoreOptions: false,
   };
 
-  async componentWillMount() {
+  async componentWillMount() {}
+
+  async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ permissionsGranted: status === 'granted' });
-  }
-
-  componentDidMount() {
     FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'photos').catch(e => {
       console.log(e, 'Directory exists');
     });
